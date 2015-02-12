af5dc3e04972cac0b87ec918904bb35e00abd19c
AlaaMuk <alaa.muk@gmail.com>
2015-02-12 11:48:16 -0500

    implement basic statuses/index

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,10 +1,9 @@
 class StatusesController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
+def index
+@statuses = Status.all
+end
+def show
+end
+def new
+end
 end
